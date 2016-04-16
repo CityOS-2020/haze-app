77c6d083372f618d9a896deed94ba49a2be73656
aleixof <fja9344@g.rit.edu>
2016-04-17 01:28:54 +0200

    new pics

--- app/src/main/java/hakaton/haze/android/hackatonapp/app/URLS.java
+++ app/src/main/java/hakaton/haze/android/hackatonapp/app/URLS.java
@@ -4,9 +4,13 @@ package hakaton.haze.android.hackatonapp.app;
  * Created by mateosokac on 08/04/16.
  */
 public interface URLS {
-    String SERVER_URL_Login = "http://10.0.2.2:47539/HackatonServolino/login";
+    /*String SERVER_URL_Login = "http://10.0.2.2:47539/HackatonServolino/login";
     String SERVER_URL_Signup = "http://10.0.2.2:47539/HackatonServolino/signup";
     String SERVER_URL_SignupFacebook = "http://10.0.2.2:47539/HackatonServolino/FacebookSignup";
+*/
+    String SERVER_URL_Login = "http://localhost:8080/HackatonServolino/login";
+    String SERVER_URL_Signup = "http://localhost:8080/HackatonServolino/signup";
+    String SERVER_URL_SignupFacebook = "http://localhost:8080/HackatonServolino/FacebookSignup";
 
 
 }
